de004804b06568a99e243855d5469ac2fb965445
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-01-28 10:01:41 +0700

    atur router to dashboard page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+
 import ReactDOM from 'react-dom/client'
 import App from './component/App'
 import './assets/index.css'
+import Dashboard from './component/Dashboard'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
